bc20d2800b791d5e4adbb384bb8e8ac75a437a6c
Роман Шевченко <webgetup@gmail.com>
2019-03-08 13:29:33 +0300

    created branch

--- src/components/app.js
+++ src/components/app.js
@@ -6,7 +6,8 @@ class App extends Component {
   render() {
     return (
       <div>
-        <h1>My React App!</h1>
+        <h1>Hello world!!!</h1>
+        <p>Start project</p>
       </div>
     );
   }
