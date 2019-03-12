cdf27e65ca1551cc1affe9ec1cea635a0e4ba7cb
Роман Шевченко <webgetup@gmail.com>
2019-03-13 00:56:12 +0300

    added bootstrap grid

--- src/components/app.js
+++ src/components/app.js
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 import '../styles/App.css';
 
@@ -8,6 +11,21 @@ class App extends Component {
       <div>
         <h1>Hello world!!!</h1>
         <p>Start project</p>
+        <div>
+          <Container>
+            <Row>
+              <Col md={4}>md=4</Col>
+              <Col md={{ span: 4, offset: 4 }}>{`md={{ span: 4, offset: 4 }}`}</Col>
+            </Row>
+            <Row>
+              <Col md={{ span: 3, offset: 3 }}>{`md={{ span: 3, offset: 3 }}`}</Col>
+              <Col md={{ span: 3, offset: 3 }}>{`md={{ span: 3, offset: 3 }}`}</Col>
+            </Row>
+            <Row>
+              <Col md={{ span: 6, offset: 3 }}>{`md={{ span: 6, offset: 3 }}`}</Col>
+            </Row>
+          </Container>
+        </div>
       </div>
     );
   }
