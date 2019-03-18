647bcfd096ab90e8c7718edffe930324fc05d286
Роман Шевченко <webgetup@gmail.com>
2019-03-18 23:01:50 +0300

    ErrorBoundary

--- src/components/app.js
+++ src/components/app.js
@@ -1,47 +1,21 @@
-
-import React, { Component } from "react";
+import React, {Component} from "react";
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+import Hero from './hero'
+import ErrorBoundary from './functions/ErrorBoundary'
 
 import '../styles/App.css';
 
-const element = React.createElement;
-
-const Hello = () => {
-    return element('div', { className: 'header' },
-    [
-      element('h1', { key: 'title' }, 'Hello world'),
-      element('h2', { className: 'small-h', key: 'subtitle' }, 'Start work with ReactJS'),
-      element('p', { className: 'text', key: 'text' }, 'Simple text')
-    ]
-  );
-};
-
 class App extends React.Component {
-  state = {
-    count: 1
-  }
-  render(){
+  render() {
     return (
-      <div className="header">
-        <h1>Hello world!!!</h1>
-        <p>Start project</p>
-        <div>
-          <Container>
-            <Row className="testBlock">
-              <Col md={4}>md=4</Col>
-              <Col md={{ span: 4, offset: 4 }}>{`md={{ span: 4, offset: 4 }}`}</Col>
-            </Row>
-            <Row className="testBlock">
-              <Col md={{ span: 3, offset: 3 }}>{`md={{ span: 3, offset: 3 }}`}</Col>
-              <Col md={{ span: 3, offset: 3 }}>{`md={{ span: 3, offset: 3 }}`}</Col>
-            </Row>
-            <Row className="testBlock">
-              <Col md={{ span: 6, offset: 3 }}>{`md={{ span: 6, offset: 3 }}`}</Col>
-            </Row>
-          </Container>
-        </div>
+      <div className="App">
+        <ErrorBoundary>
+          <Hero heroName="Batman"/>
+          <Hero heroName="Superman"/>
+          <Hero heroName="Joker"/>
+        </ErrorBoundary>
       </div>
     );
   }
