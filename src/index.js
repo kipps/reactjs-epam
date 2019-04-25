3c153e2212fd06e1a2b0afa7eab01d202e693fe4
Roman <roman_shevchenko@epam.com>
2019-04-25 21:51:21 +0300

    routher

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
-import store from './redux/store/configureStore'
+import store from './redux/store/configureStore';
+import {BrowserRouter} from 'react-router-dom';
 
 import App from "./container/app.js";
 
 ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
-    document.getElementById('root')
+  <Provider store={store}>
+    <BrowserRouter>
+      <App/>
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById('root')
 )
