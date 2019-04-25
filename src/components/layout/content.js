6704d9b68eb3d07f19680dc1fde440e39ea8e2a5
PETERSBURG\Roman_Shevchenko <Roman_Shevchenko@epam.com>
2019-04-25 17:07:15 +0300

    start work on routing

--- src/components/layout/content.js
+++ src/components/layout/content.js
@@ -1,16 +1,24 @@
 import React from 'react';
-import SearchResult from '../search-result/SearchResult'
+import {Switch, Route} from 'react-router-dom'
 import MoviesList from '../movies-list/MoviesList'
+import PageNotFoundComponent from "../page404/PageNotFoundComponent";
+import MovieComponent from "../movie/MovieComponent";
 
 class Content extends React.Component {
-  render() {
-    return (
-      <div className='Content'>
-        <div className='pt-24 pb-24'>
-          <MoviesList/>
-        </div>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className='Content'>
+                <div className='pt-24 pb-24'>
+                    <Switch>
+                        <Route exact path='/' component={MoviesList}/>
+                        <Route path='/film' component={MovieComponent}/>
+                        <Route path='/search' component={MoviesList}/>
+                        <Route path='/page-404' component={PageNotFoundComponent}/>
+                    </Switch>
+                </div>
+            </div>
+        );
+    }
 }
+
 export default Content;
---
+++ src/components/movie/FullMoiveComponent.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const FullMoiveComponent = () => {
+    return <p>FullMoiveComponent</p>
+}
+
+export default FullMoiveComponent;
