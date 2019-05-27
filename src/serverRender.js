121e0b9666ad1e05b591cd2c76984e4850e75b2d
PETERSBURG\Roman_Shevchenko <Roman_Shevchenko@epam.com>
2019-05-27 18:41:47 +0300

    add server's files

---
+++ src/serverRender.js
@@ -0,0 +1,19 @@
+const renderHTML = () => {
+    return `
+        <!doctype html>
+        <html>
+            <head>
+                <meta charset=utf-8 />
+                <title>Movies for fun</title>
+            </head>
+            <body>
+                <div id="root"></div>
+                <script src="/js/main.js"></script>
+            </body>
+        </html>
+    `;
+}
+
+module.exports = (req, res) => {
+    res.send(renderHTML());
+}
